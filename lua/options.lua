a95ca0edc13f428a43eaad89de81b38b72f2de0a
Andrew Carson <andrew.carson@jigger.me.uk>
2025-10-16 21:31:47 +0100

    enable relative line numbers and disable the mouse

--- lua/options.lua
+++ lua/options.lua
@@ -7,10 +7,10 @@
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+-- Disable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
